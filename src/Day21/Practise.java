914de05881f0a673ed2c49bf5d265d3818d5d49e
shahimilan-72 <79278603+shahimilan-72@users.noreply.github.com>
2022-12-17 20:21:09 -0600

    change made by branch2

--- src/Day21/Practise.java
+++ src/Day21/Practise.java
@@ -3,5 +3,6 @@ package Day21;
 public class Practise {
     public static void main(String[] args) {
         System.out.println(" nothing");
+        System.out.println("--------------");
     }
 }
