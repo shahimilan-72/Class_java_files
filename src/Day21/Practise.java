ea47ad45e0e0329beac930286db774f1e0a10b8e
shahimilan-72 <79278603+shahimilan-72@users.noreply.github.com>
2022-12-17 19:35:22 -0600

    change made by branch1

--- src/Day21/Practise.java
+++ src/Day21/Practise.java
@@ -2,7 +2,7 @@ package Day21;
 
 public class Practise {
     public static void main(String[] args) {
-        System.out.println("----------------");
+        System.out.println("change made by branch 1");
 
     }
 }
